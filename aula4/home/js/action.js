93fb92ca32a3c5dfd3b240740ed8da29ffe20a3b
James Alves <james@jamesalves.com.br>
2025-04-02 21:05:41 -0300

    add fetch operations

--- aula4/home/js/action.js
+++ aula4/home/js/action.js
@@ -1,5 +1,5 @@
 const emain = document.querySelector('main');
-
+/// FAZER UM GET
 fetch('https://jsonplaceholder.typicode.com/users').then((res)=>{
     return res.json();
 }).then((data)=>{
@@ -9,3 +9,58 @@ fetch('https://jsonplaceholder.typicode.com/users').then((res)=>{
         emain.appendChild(ediv)
     });
 })
+
+
+const postBlog = {
+    userId: 1,
+    title: "Como passar de ano",
+    body: "basta apenas estudar"
+}
+const execResponse = (res) => {
+    console.log("resposta do post")
+
+console.log(res.status)
+    console.log(res.ok)
+
+}
+
+const execResponse2 = (res) => {
+    
+    console.log("resposta do put exec na linha 47")
+
+console.log(res.status)
+    console.log(res.ok)
+
+}
+
+
+fetch("https://jsonplaceholder.typicode.com/posts", {
+    body: JSON.stringify(postBlog),
+    method: "POST",
+    headers: {'Content-type': 'application/json; charset=UTF-8',}
+}).then(execResponse)
+
+
+fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    body: JSON.stringify(postBlog),
+    method: "PUT",
+    headers: {'Content-type': 'application/json; charset=UTF-8',}
+}).then(execResponse2)
+
+console.log("executado na linha 49")
+
+const acDelete = fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "DELETE",
+})
+
+acDelete.then(execResponse2);
+
+
+
+async function executa3(){
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const listaUsers = await response.json();
+    console.log(listaUsers);
+}
+
+executa3();
